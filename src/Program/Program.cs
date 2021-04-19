26e8d3ad1064d807bde2c21626734b4ba60b179e
Cotidib <constanza.dibueno@gmail.com>
2021-04-19 16:59:23 -0300

    Modifico clase program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,10 +8,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Person john = new Person("John Doe", "1.234.567-2");
-            Person jane = new Person("Jane Doe", "8.765.432-7");
+            Person john = new Person("John Doe", "1.234.567-2"); //"1.234.567-2" True
             john.IntroduceYourself();
-            jane.IntroduceYourself();  
+            Person jane = new Person("Jane Doe", "8.765.432-7"); //"8.765.432-7" True
+            jane.IntroduceYourself();
+            Person juan = new Person("", "8.765.432-8"); 
+            juan.IntroduceYourself();
+            Person julio = new Person("Julio Doe", "8.765.432-5");
+            julio.IntroduceYourself();
+            Person juliana = new Person("","1.234.567-2");
+            juliana.IntroduceYourself();  
         }
     }
 }
